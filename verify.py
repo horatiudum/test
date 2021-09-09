27509b3245de368d1fa4a12a9243f1c339da6051
Nobody <nobody@example.com>
2021-09-09 09:48:38 +0300

    h mesaj1

--- verify.py
+++ verify.py
@@ -2,7 +2,7 @@ import uuid
 import pandas as pd
 df = pd.read_csv('test.csv')
 df.reset_index(inplace=True)
-print(df.columns)
+#print(df.columns)
 print(df.groupby('uuid').count())
 print([uuid.uuid4() for x in range(1, 10)])
 
